b9af641075d3eaccdc52f3c0c9cae7c10c33d21b
Richard To <gohn76@gmail.com>
2012-02-05 03:19:21 -0900

    Added template

---
+++ template/template.s
@@ -0,0 +1,18 @@
+#################################################################################
+#
+#	Richard To
+#	rto@alaska.edu
+#	Programming assignment number
+#	Date
+#
+#	PURPOSE:		A high-level description of WHAT your program computes.
+#
+#	ALGORITHM:		A description of HOW this computation is performed.
+#
+#	INPUTS:		A list and description of each of the inputs used by your  
+#				program, including values read directly from memory.
+#
+#	OUTPUTS:		A list and description of each of the outputs produced by
+#				your program, including values directly stored in memory.
+#
+#################################################################################
