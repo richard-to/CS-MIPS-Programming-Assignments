3071b90019070fbbf45a604ff6d3faf2fd0de1ce
Richard To <gohn76@gmail.com>
2012-04-12 22:00:52 -0800

    Added comments to code

--- assignment4/prgm1.s
+++ assignment4/prgm1.s
@@ -120,228 +120,228 @@ INPUT:            .space 80
 
                   .text
 main:
-    addi  $s5, $zero, 89
-    la    $a0, INPUT_PROMPT
-    jal   putString    
-    
-    la    $s0, INPUT
-    add   $a0, $zero $s0
-    jal   getString
-
-mainBadSyntax:
-    add   $a0, $s0, $zero
-    addi  $sp, $sp, -4
-    sw    $ra, 0($sp)
-    jal   badSyntax
-    lw    $ra, 0($sp)
-    addi  $sp, $sp, 4
-    add   $s1, $v0, $zero
-    beq   $s1, $zero, mainStackUnderflow
-    la    $a0, BAD_SYNTAX
-    jal   putString     
-    j     mainContinue
-    
-mainStackUnderflow:
-    add   $a0, $s0, $zero
-    addi  $sp, $sp, -4
-    sw    $ra, 0($sp)
-    jal   stackUnderflow
-    lw    $ra, 0($sp)
-    addi  $sp, $sp, 4
-    add   $s1, $v0, $zero
-    beq   $s1, $zero, mainTooManyOperands
-    la    $a0, STACK_UNDERFLOW
-    jal   putString
-    j     mainContinue
-    
-mainTooManyOperands:
-    add   $a0, $s0, $zero
-    addi  $sp, $sp, -4
-    sw    $ra, 0($sp)
-    jal   tooManyOperands
-    lw    $ra, 0($sp)
-    addi  $sp, $sp, 4
-    add   $s1, $v0, $zero
-    beq   $s1, $zero, mainContinue
-    la    $a0, TOO_MANY_OPS
-    jal   putString
-        
-mainContinue:
-    la    $a0, CONTINUE_PROMPT
-    jal   putString
-    
-    add   $a0, $zero $s0
-    jal   getString     
-            
-    lb      $s4, 0($a0)                    
-    beq     $s4, $s5, main
-    
-mainEnd:                                                                       
-    li      $v0, 10                        
-    syscall
-    
-putString:
-    li      $v0, 4            
-    syscall
-    jr       $ra
-
-getString:
-    addi    $a1, $zero, 80                         
-    li      $v0, 8                         
-    syscall
-    jr      $ra
-                                        
-badSyntax:
-    addi  $sp, $sp -8
-    sw    $ra, 0($sp)
-    sw    $a0, -4($sp)
-    add   $t0, $a0, $zero
-    addi  $t1, $zero, 10
-    addi  $t2, $zero, 32
-    addi  $t4, $zero, 0
-bsLoop:
-    lb    $t3, 0($t0)
-    beq   $t3, $t1, bsEnd
-    addi  $t7, $zero, 32
-    beq   $t3, $t7, bsIncr
-    addi  $t7, $zero, 42
-    beq   $t3, $t7, bsIsOperator
-    addi  $t7, $zero, 43
-    beq   $t3, $t7, bsIsOperator
-    addi  $t7, $zero, 45
-    beq   $t3, $t7, bsIsOperator
-    addi  $t7, $zero, 47
-    beq   $t3, $t7, bsIsOperator   
-    slti  $t7, $t3, 48
-    bne   $t7, $zero, bsFailEnd
-    slti  $t7, $t3, 58
-    beq   $t7, $zero, bsFailEnd
-bsIsDigit:
-    addi  $t7, $zero, 32
-    beq   $t2, $t7, bsIncr
-    addi  $t7, $zero, 45
-    beq   $t2, $t7, bsIncr
-    slti  $t7, $t2, 48
-    bne   $t7, $zero, bsFailEnd
-    slti  $t7, $t2, 58
-    beq   $t7, $zero, bsFailEnd       
-    j     bsIncr                 
-bsIsOperator:
-    addi  $t7, $zero, 32
-    bne   $t2, $t7, bsFailEnd          
-bsIncr:
-    add   $t2, $t3, $zero    
-    addi  $t0, $t0, 1
-    j     bsLoop
-bsFailEnd:
-    addi  $t4, $zero, 1    
-bsEnd:
-    lw    $a0, 4($sp)
-    lw    $ra, 0($sp)
-    addi  $sp, $sp, 4
-    add   $v0, $zero, $t4
-    jr    $ra
-    
-
-
-stackUnderflow:
-    addi  $sp, $sp -8
-    sw    $ra, 0($sp)
-    sw    $a0, -4($sp)
-    add   $t0, $a0, $zero
-    addi  $t1, $zero, 10
-    add   $t2, $zero, $zero
-    add   $t3, $zero, $zero
-    add   $t4, $zero, $zero    
-suLoop:
-    lb    $t5, 0($t0)
-    beq   $t5, $t1, suEnd
-    lb    $t6, 1($t0)
-    beq   $t6, $t1, suNextIsSpace
-    addi  $t7, $zero, 32
-    bne   $t6, $t7, suIncr
-suNextIsSpace:
-    addi  $t7, $zero, 42
-    beq   $t5, $t7, suIsOperator
-    addi  $t7, $zero, 43
-    beq   $t5, $t7, suIsOperator
-    addi  $t7, $zero, 45
-    beq   $t5, $t7, suIsOperator
-    addi  $t7, $zero, 47
-    beq   $t5, $t7, suIsOperator
-    slti  $t7, $t5, 48
-    bne   $t7, $zero, suIncr
-    slti  $t7, $t5, 58
-    beq   $t7, $zero, suIncr
-suIsOperand:
-    addi  $t3, $t3, 1
-    j     suCheckValid        
-suIsOperator:
-    addi  $t4, $t4, 1
-suCheckValid:
-    add   $t7, $t3, $t4
-    beq   $t7, $zero, suIncr
-    subu  $t7, $t3, $t4
-    blez  $t7, suFailEnd               
-suIncr:    
-    addi  $t0, $t0, 1
-    j     suLoop
-suFailEnd:
-    addi  $t2, $zero, 1    
-suEnd:
-    lw    $a0, 4($sp)
-    lw    $ra, 0($sp)
-    addi  $sp, $sp, 4
-    add   $v0, $zero, $t2
-    jr    $ra
-    
-    
-    
-tooManyOperands:
-    addi  $sp, $sp -8
-    sw    $ra, 0($sp)
-    sw    $a0, -4($sp)
-    add   $t0, $a0, $zero
-    addi  $t1, $zero, 10
-    add   $t2, $zero, $zero
-    add   $t3, $zero, $zero
-    add   $t4, $zero, $zero    
-tmoLoop:
-    lb    $t5, 0($t0)
-    beq   $t5, $t1, tmoCheckValid
-    lb    $t6, 1($t0)
-    beq   $t6, $t1, tmoNextIsSpace
-    addi  $t7, $zero, 32
-    bne   $t6, $t7, tmoIncr
-tmoNextIsSpace:
-    addi  $t7, $zero, 42
-    beq   $t5, $t7, tmoIsOperator
-    addi  $t7, $zero, 43
-    beq   $t5, $t7, tmoIsOperator
-    addi  $t7, $zero, 45
-    beq   $t5, $t7, tmoIsOperator
-    addi  $t7, $zero, 47
-    beq   $t5, $t7, tmoIsOperator
-    slti  $t7, $t5, 48
-    bne   $t7, $zero, tmoIncr
-    slti  $t7, $t5, 58
-    beq   $t7, $zero, tmoIncr
-tmoIsOperand:
-    addi  $t3, $t3, 1
-    j     tmoIncr        
-tmoIsOperator:
-    addi  $t4, $t4, 1             
-tmoIncr:    
-    addi  $t0, $t0, 1
-    j     tmoLoop
-tmoCheckValid:
-    subu  $t7, $t3, $t4
-    addiu $t7, $t7, -1
-    blez  $t7, tmoEnd
-    add   $t2, $zero, 1   
-tmoEnd: 
-    lw    $a0, 4($sp)
-    lw    $ra, 0($sp)
-    addi  $sp, $sp, 4
-    add   $v0, $zero, $t2
-    jr    $ra                              
+    addi  $s5, $zero, 89                    # while go_again == "Y":
+    la    $a0, INPUT_PROMPT                 # go_again = "Y"
+    jal   putString                         # prompt = "Enter a postfix expression: "
+                                            # putString(prompt)    
+    la    $s0, INPUT                        #
+    add   $a0, $zero $s0                    #
+    jal   getString                         # getString(S)
+                                            # 
+mainBadSyntax:                              # 
+    add   $a0, $s0, $zero                   #
+    addi  $sp, $sp, -4                      # 
+    sw    $ra, 0($sp)                       #
+    jal   badSyntax                         # invalid = badSyntax(S)
+    lw    $ra, 0($sp)                       # 
+    addi  $sp, $sp, 4                       #
+    add   $s1, $v0, $zero                   #
+    beq   $s1, $zero, mainStackUnderflow    # if invalid:
+    la    $a0, BAD_SYNTAX                   # putString("Error:Bad Syntax")    
+    jal   putString                         #  
+    j     mainContinue                      #
+                                            #
+mainStackUnderflow:                         #
+    add   $a0, $s0, $zero                   #
+    addi  $sp, $sp, -4                      #
+    sw    $ra, 0($sp)                       #
+    jal   stackUnderflow                    # invalid = stackUnderflow(S)
+    lw    $ra, 0($sp)                       #
+    addi  $sp, $sp, 4                       #
+    add   $s1, $v0, $zero                   #
+    beq   $s1, $zero, mainTooManyOperands   # if invalid:
+    la    $a0, STACK_UNDERFLOW              # putString("Error:Stack Underflow") 
+    jal   putString                         # 
+    j     mainContinue                      #
+                                            #
+mainTooManyOperands:                        # 
+    add   $a0, $s0, $zero                   #
+    addi  $sp, $sp, -4                      # 
+    sw    $ra, 0($sp)                       #
+    jal   tooManyOperands                   # invalid = tooManyOperands(S)
+    lw    $ra, 0($sp)                       #
+    addi  $sp, $sp, 4                       #
+    add   $s1, $v0, $zero                   #
+    beq   $s1, $zero, mainContinue          # if invalid:
+    la    $a0, TOO_MANY_OPS                 # putString("Error:Too many Operands") 
+    jal   putString                         #   
+                                            # 
+mainContinue:                               #
+    la    $a0, CONTINUE_PROMPT              # 
+    jal   putString                         # putString("Enter another postfix expression? (Y/n) ")
+                                            #
+    add   $a0, $zero $s0                    #
+    jal   getString                         # getString(go_again)
+                                            # 
+    lb      $s4, 0($a0)                     #
+    beq     $s4, $s5, main                  # if go_again != "Y":
+                                            # break
+mainEnd:                                    #                                        
+    li      $v0, 10                         #
+    syscall                                 # Exit 
+                                            #
+putString:                                  # def putString(S):
+    li      $v0, 4                          # print S
+    syscall                                 #
+    jr       $ra                            #
+                                            #
+getString:                                  # def getString(S):
+    addi    $a1, $zero, 80                  # raw_input(S)       
+    li      $v0, 8                          #
+    syscall                                 #
+    jr      $ra                             #
+                                            #
+badSyntax:                                  # def badSyntax(S):
+    addi  $sp, $sp -8                       #
+    sw    $ra, 0($sp)                       #
+    sw    $a0, -4($sp)                      #
+    add   $t0, $a0, $zero                   # 
+    addi  $t1, $zero, 10                    # newline = "\n"
+    addi  $t2, $zero, 32                    # space = ' '
+    addi  $t4, $zero, 0                     # invalid = 0
+bsLoop:                                     # for i in range(length):
+    lb    $t3, 0($t0)                       #
+    beq   $t3, $t1, bsEnd                   #
+    addi  $t7, $zero, 32                    # if(curr == ' '):
+    beq   $t3, $t7, bsIncr                  # is_valid = True
+    addi  $t7, $zero, 42                    # 
+    beq   $t3, $t7, bsIsOperator            # elif(curr == "*"):
+    addi  $t7, $zero, 43                    #
+    beq   $t3, $t7, bsIsOperator            # elif(curr == "+"):
+    addi  $t7, $zero, 45                    # 
+    beq   $t3, $t7, bsIsOperator            # elif(curr == "-"):
+    addi  $t7, $zero, 47                    #
+    beq   $t3, $t7, bsIsOperator            # elif(curr == "/"):
+    slti  $t7, $t3, 48                      #
+    bne   $t7, $zero, bsFailEnd             # elif(re.search('^[\d]$', curr) != None):
+    slti  $t7, $t3, 58                      #
+    beq   $t7, $zero, bsFailEnd             #
+bsIsDigit:                                  #
+    addi  $t7, $zero, 32                    # 
+    beq   $t2, $t7, bsIncr                  # elif(prev == ' '):
+    addi  $t7, $zero, 45                    #
+    beq   $t2, $t7, bsIncr                  # elif(prev == '-'):
+    slti  $t7, $t2, 48                      #
+    bne   $t7, $zero, bsFailEnd             # elif(re.search('^[\d]$', prev) != None):
+    slti  $t7, $t2, 58                      #
+    beq   $t7, $zero, bsFailEnd             #
+    j     bsIncr                            #
+bsIsOperator:                               #
+    addi  $t7, $zero, 32                    # 
+    bne   $t2, $t7, bsFailEnd               # elif(prev == ' '):
+bsIncr:                                     #
+    add   $t2, $t3, $zero                   # 
+    addi  $t0, $t0, 1                       #
+    j     bsLoop                            #
+bsFailEnd:                                  #
+    addi  $t4, $zero, 1                     # invalid = 1
+bsEnd:                                      #
+    lw    $a0, 4($sp)                       # 
+    lw    $ra, 0($sp)                       #
+    addi  $sp, $sp, 4                       # 
+    add   $v0, $zero, $t4                   #
+    jr    $ra                               # return invalid
+                                            #
+                                            #
+                                            #
+stackUnderflow:                             # def stackUnderflow(S):
+    addi  $sp, $sp -8                       #
+    sw    $ra, 0($sp)                       #
+    sw    $a0, -4($sp)                      #
+    add   $t0, $a0, $zero                   # 
+    addi  $t1, $zero, 10                    # newline = "\n"
+    add   $t2, $zero, $zero                 # invalid = 0
+    add   $t3, $zero, $zero                 # operands = 0
+    add   $t4, $zero, $zero                 # operators = 0
+suLoop:                                     # for i in range(length):
+    lb    $t5, 0($t0)                       #
+    beq   $t5, $t1, suEnd                   #
+    lb    $t6, 1($t0)                       #
+    beq   $t6, $t1, suNextIsSpace           # if i+1 < length:
+    addi  $t7, $zero, 32                    #
+    bne   $t6, $t7, suIncr                  # 
+suNextIsSpace:                              # if next == ' ':
+    addi  $t7, $zero, 42                    #
+    beq   $t5, $t7, suIsOperator            # elif(curr == "*"): 
+    addi  $t7, $zero, 43                    #
+    beq   $t5, $t7, suIsOperator            # elif(curr == "+"):
+    addi  $t7, $zero, 45                    #  
+    beq   $t5, $t7, suIsOperator            # elif(curr == "-"):
+    addi  $t7, $zero, 47                    #
+    beq   $t5, $t7, suIsOperator            # elif(curr == "/"):
+    slti  $t7, $t5, 48                      #  
+    bne   $t7, $zero, suIncr                # elif re.search('^[\d]$', curr) != None:
+    slti  $t7, $t5, 58                      #
+    beq   $t7, $zero, suIncr                #
+suIsOperand:                                #
+    addi  $t3, $t3, 1                       # operands += 1
+    j     suCheckValid                      # 
+suIsOperator:                               #
+    addi  $t4, $t4, 1                       # operators += 1
+suCheckValid:                               #
+    add   $t7, $t3, $t4                     #
+    beq   $t7, $zero, suIncr                # if (operators > 0 or operands > 0) 
+    subu  $t7, $t3, $t4                     #
+    blez  $t7, suFailEnd                    # and operands <= operators:
+suIncr:                                     #
+    addi  $t0, $t0, 1                       #
+    j     suLoop                            # 
+suFailEnd:                                  #    
+    addi  $t2, $zero, 1                     # invalid = 1
+suEnd:                                      # 
+    lw    $a0, 4($sp)                       #    
+    lw    $ra, 0($sp)                       #
+    addi  $sp, $sp, 4                       #
+    add   $v0, $zero, $t2                   #
+    jr    $ra                               # return invalid  
+                                            # 
+                                            #
+                                            #
+tooManyOperands:                            # def tooManyOperands(S):
+    addi  $sp, $sp -8                       #
+    sw    $ra, 0($sp)                       # 
+    sw    $a0, -4($sp)                      #  
+    add   $t0, $a0, $zero                   #
+    addi  $t1, $zero, 10                    #
+    add   $t2, $zero, $zero                 #
+    add   $t3, $zero, $zero                 # operands = 0
+    add   $t4, $zero, $zero                 # operators = 0
+tmoLoop:                                    # for i in range(length):
+    lb    $t5, 0($t0)                       #
+    beq   $t5, $t1, tmoCheckValid           # 
+    lb    $t6, 1($t0)                       #
+    beq   $t6, $t1, tmoNextIsSpace          # if i+1 == length:
+    addi  $t7, $zero, 32                    #     
+    bne   $t6, $t7, tmoIncr                 #
+tmoNextIsSpace:                             # if next == ' ':
+    addi  $t7, $zero, 42                    #
+    beq   $t5, $t7, tmoIsOperator           # elif(curr == "*"): 
+    addi  $t7, $zero, 43                    #
+    beq   $t5, $t7, tmoIsOperator           # elif(curr == "+"): 
+    addi  $t7, $zero, 45                    #
+    beq   $t5, $t7, tmoIsOperator           # elif(curr == "-"): 
+    addi  $t7, $zero, 47                    #
+    beq   $t5, $t7, tmoIsOperator           # elif(curr == "/"): 
+    slti  $t7, $t5, 48                      #
+    bne   $t7, $zero, tmoIncr               # elif re.search('^[+-/*]$', curr) != None:
+    slti  $t7, $t5, 58                      #
+    beq   $t7, $zero, tmoIncr               #
+tmoIsOperand:                               # 
+    addi  $t3, $t3, 1                       # operands += 1
+    j     tmoIncr                           #
+tmoIsOperator:                              #
+    addi  $t4, $t4, 1                       # operators += 1
+tmoIncr:                                    # 
+    addi  $t0, $t0, 1                       #
+    j     tmoLoop                           #
+tmoCheckValid:                              # 
+    subu  $t7, $t3, $t4                     # 
+    addiu $t7, $t7, -1                      #
+    blez  $t7, tmoEnd                       # if operands - operators > 1:
+    add   $t2, $zero, 1                     # invalid = 1
+tmoEnd:                                     #
+    lw    $a0, 4($sp)                       #
+    lw    $ra, 0($sp)                       #
+    addi  $sp, $sp, 4                       #   
+    add   $v0, $zero, $t2                   # return invalid
+    jr    $ra                               #
